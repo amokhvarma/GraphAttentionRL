77f29c7007cf4ccca8f1c42a0758651cf7e0473d
Achint Aggarwal <achint.aggarwal11@gmail.com>
2021-05-09 16:51:59 +0530

    Added rewards' output

--- main.py
+++ main.py
@@ -14,13 +14,15 @@ parser.add_argument('--batch_size', dest='batch_size', default=32, type=int,
 args = parser.parse_args()
 
 number_of_games = args.number_of_games
-Agent = DQN()
+Agent = DQN('GAT')
 total_episodes = 0
 batch_size = args.batch_size
 for i in range(0,number_of_games):
     env = gym.make("MsPacman-v0")
     state = env.reset()
     done = False
+    temp = []
+    this_game_episodes = 0
     while(not done):
         old_state = state
         feat1 = make_graph(old_state,50)
@@ -29,6 +31,13 @@ for i in range(0,number_of_games):
         feat2 = make_graph(next_state,50)
         Agent.remember(feat1,action,reward,feat2,done)
         total_episodes+=1
-        if(total_episodes%batch_size == 0):
-            print(total_episodes)
-            print(Agent.replay())
+        this_game_episodes+=1
+        temp.append(reward)
+        if(total_episodes % 100 == 0):
+          arr = temp[len(temp)-100: len(temp)]
+          print(sum(arr), ", episode = ", this_game_episodes)  
+    print("This Game Episodes - ", this_game_episodes)      
+    print("*****GAME OVER****")
+        # if(total_episodes%batch_size == 0):
+        #     print(total_episodes)
+        #     print(Agent.replay())
